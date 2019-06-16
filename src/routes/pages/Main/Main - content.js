d5238b9ba19a0a85011721064543e90c6b8999ef
LiprikON <kpyton2@gmail.com>
2019-06-16 22:19:19 +0300

    WIP image added

--- src/routes/pages/Main/Main - content.js
+++ src/routes/pages/Main/Main - content.js
@@ -6,6 +6,7 @@ export default class Content extends React.Component {
     render() {
         return (
             <section className="container">
+                <img className="WIP" src="css/img/Work-In-Progress.png" alt="Work in progress"/>
             </section>
         )
     }
