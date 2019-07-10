be0d2340bd16cc86f8176ff17dd309ee0a5d7d40
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2019-07-10 19:41:43 +0300

    fixed 404 page for new grid

--- src/routes/pages/404/404 - content.js
+++ src/routes/pages/404/404 - content.js
@@ -10,9 +10,11 @@ class Content extends React.Component {
     render() {
         return (
             <section className="container">
-                <h1 className="title big er404">404</h1>
-                <h4 className="title er404">We couldn't find <br/> this page.</h4>
-                <button className="button back"onClick={this.goBack}><i class="fas fa-chevron-circle-left"/> GO BACK</button>
+                <div className="er404 holder">
+                    <h1 className="er404 big text">404</h1>
+                    <h4 className="er404 text">We couldn't find <br/> this page.</h4>
+                    <button className="er404 button back"onClick={this.goBack}><i class="fas fa-chevron-circle-left"/> GO BACK</button>
+                </div>
             </section>
         )
     }
--- src/routes/pages/Home/Home - content.js
+++ src/routes/pages/Home/Home - content.js
@@ -1,33 +1,14 @@
 import React from 'react'
+import Clock from 'react-live-clock'
 
 export default class Content extends React.Component {
-    constructor() {
-        super()
-        this.renderTime = this.renderTime.bind(this)
-        this.state = {time: "00:00:00"}
-    }
-    componentDidMount() {
-        this.time = setInterval(this.renderTime, 1000)
-    }
-    componentWillUnmount() {
-        clearInterval(this.time)
-    }
-    renderTime() {
-        const now = new Date()
 
-        // Formating time to 00 format (9 -> 09)
-        const hour = ("0" +now.getHours()).slice(-2)
-        const minute = ("0" +now.getMinutes()).slice(-2)
-        const second = ("0" +now.getSeconds()).slice(-2)
-        
-        this.setState({time: `${hour}:${minute}:${second}`})
-    }
     render() {
         return (
             <section className="container">
                 <h6 className="title fancy">
                     <p>The Fanciest Clock in the Universe!</p>
-                        <div className="timer">{this.state.time}</div>
+                        <Clock className="timer" ticking="true" format="HH:mm:ss"/>
                     <br/>
                 </h6>
                 <img src="css/img/custom-hand.png" alt="Hand with a clock" className="hand-img"/>
