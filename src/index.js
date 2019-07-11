049f5e7264428d15f092f726aeb4ea560caa047d
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2019-07-11 23:02:46 +0300

    Animated Favicon!

--- src/index.js
+++ src/index.js
@@ -6,12 +6,32 @@ import * as serviceWorker from './serviceWorker';
 
 import Favicon from 'react-favicon'
 
+// importing favicon animation
+// ref https://stackoverflow.com/questions/42118296/dynamically-import-images-from-a-directory-using-webpack
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+function makeImageArray() {
+    const images = importAll(require.context('./Clock Anim', false, /\.png/));
+    const arr = []
+    for (let key in images) {
+        arr.push(images[key])
+    }
+    return arr
+}
+
+
 ReactDOM.render(
         <BrowserRouter>
-            <Favicon url={["http://individual.icons-land.com/IconsPreview/BaseSoftware/PNG/256x256/AlarmClock2.png", "https://www.favicon-generator.org/download/2014-12-26/6cb68d84a7d49e5a39a296d9b0e7ade3.ico", "https://upload.wikimedia.org/wikipedia/commons/3/37/Clock.gif"]}/>
+            <Favicon url={makeImageArray()}/>
             <App />
         </BrowserRouter>,
     document.getElementById('root'));
+
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/routes/pages/Home/Home - content.js
+++ src/routes/pages/Home/Home - content.js
@@ -8,7 +8,7 @@ export default class Content extends React.Component {
             <section className="container">
                 <h6 className="title fancy">
                     <p>The Fanciest Clock in the Universe!</p>
-                        <Clock className="timer" ticking="true" format="HH:mm:ss"/>
+                        <Clock className="timer" ticking={true} format="HH:mm:ss"/>
                     <br/>
                 </h6>
                 <img src="css/img/custom-hand.png" alt="Hand with a clock" className="hand-img"/>
