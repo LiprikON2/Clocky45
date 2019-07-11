4eea4f1b4813c375a8ac74d26896a4dd9ef77449
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2019-07-11 21:52:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Favicon from 'react-favicon'
 
 ReactDOM.render(
         <BrowserRouter>
-            <Favicon url={["http://individual.icons-land.com/IconsPreview/BaseSoftware/PNG/256x256/AlarmClock2.png", "https://www.favicon-generator.org/download/2014-12-26/6cb68d84a7d49e5a39a296d9b0e7ade3.ico"]}/>
+            <Favicon url={["http://individual.icons-land.com/IconsPreview/BaseSoftware/PNG/256x256/AlarmClock2.png", "https://www.favicon-generator.org/download/2014-12-26/6cb68d84a7d49e5a39a296d9b0e7ade3.ico", "https://upload.wikimedia.org/wikipedia/commons/3/37/Clock.gif"]}/>
             <App />
         </BrowserRouter>,
     document.getElementById('root'));
