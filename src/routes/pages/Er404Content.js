934ace0c63e1620f3f33547ab53f4e2b3fa34de2
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2019-07-26 19:44:48 +0300

    intial mobile support for er404 button

--- src/routes/pages/Er404Content.js
+++ src/routes/pages/Er404Content.js
@@ -23,11 +23,11 @@ class Content extends React.Component {
     render() {
         return (
             <section className="container">
-                <div className="er404 holder">
-                    <h1 className="er404 big text">404</h1>
-                    <h4 className="er404 text">We couldn't find <br/> this page.</h4>
+                <div className="er404 er404-holder">
+                    <h1 className="er404 er404-big-text">404</h1>
+                    <h4 className="er404 er404-text">We couldn't find <br/> this page.</h4>
                     {/* <button className="er404 button back unselectable" onClick={this.goBack} ><i className="fas fa-chevron-circle-left"/> GO BACK</button> */}
-                    <button className='er404 button back myButt four unselectable'>
+                    <button className='er404 er404-btn button unselectable'>
                         <div className='icon fas fa-chevron-circle-left'></div>
                         <span>GO BACK</span>
                     </button>
