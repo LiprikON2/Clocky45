4791b6be7453e793487477c2f4e3fd5b4aad8a87
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2019-07-28 13:10:38 +0300

    s

--- src/routes/pages/Er404Content.js
+++ src/routes/pages/Er404Content.js
@@ -4,7 +4,15 @@ import { withRouter } from 'react-router-dom'
 class Content extends React.Component {
 
     goBack = () => {
+        // const currPath = this.props.history.location.pathname
+        // console.log(currPath);
         this.props.history.goBack()
+        // setTimeout((params) => {
+            
+            // this.props.history.push(currPath);
+        // }, 10000
+        // )
+
         
     }
 
@@ -27,7 +35,7 @@ class Content extends React.Component {
                     <h1 className="er404 er404-big-text">404</h1>
                     <h4 className="er404 er404-text">We couldn't find <br/> this page.</h4>
                     {/* <button className="er404 button back unselectable" onClick={this.goBack} ><i className="fas fa-chevron-circle-left"/> GO BACK</button> */}
-                    <button className='er404 er404-btn button unselectable'>
+                    <button className='er404 er404-btn button unselectable' onClick={this.goBack}>
                         <div className='icon fas fa-chevron-circle-left'></div>
                         <span>GO BACK</span>
                     </button>
